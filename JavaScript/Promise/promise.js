8ea533803da9bf47fea6d27844734101e3c4611a
jianhui.fang <jianhui.fang@ucarinc.com>
2020-03-28 09:39:33 +0800

    新增promise.md

--- JavaScript/Promise/promise.js
+++ JavaScript/Promise/promise.js
@@ -1,29 +1,28 @@
 // promise的立即执行性
-/** 
-let p = new Promise((resolve, reject) => {
-  console.log('create a promise');
+// let p = new Promise((resolve, reject) => {
+//   console.log('create a promise');
   
-  resolve('success')
-})
+//   resolve('success')
+// })
 
-console.log('after new Promise');
+// console.log('after new Promise');
 
-p.then(value => {
-  console.log(value);
-})
-*/
+// p.then(value => {
+//   console.log(value);
+// })
 
-// output
+// ------ output ------
 // create a promise
 // after new promise
 // success
-// 将 p.then 注释掉依然会执行console.log('create a promise')
-// 这就说明作为Promise参数的函数会被立即执行，只是其中执行的代码可以是异步代码
+/**
+ * 将 p.then 注释掉依然会执行console.log('create a promise')
+ * 这就说明仅仅是在刚创建(new) Promise 时，作为 Promise 参数被传入的函数都会被立即执行(没有调用变量p)，只是其中执行的代码可以是异步代码
+ */
 
 // -------------------------------------------------
 
 // Promise的三种状态
-/** */
 let p1 = new Promise((resolve, reject) => {
   resolve(1)
 });
@@ -38,6 +37,14 @@ let p3 = new Promise((resolve, reject) => {
   }, 500)
 })
 
-console.log(p1);
-console.log(p2);
-console.log(p3);
+console.log('cl', p1);
+console.log('cl', p2);
+console.log('cl', p3);
+
+setTimeout(() => { console.log('set', p2); }, 1000)
+setTimeout(() => { console.log('set', p3); }, 1000)
+
+p1.then(value => console.log('then', value))
+p2.then(value => console.log('then', value))
+p3.catch(err => console.log('then', err))
+// p3.then(value => console.log('then', value))
