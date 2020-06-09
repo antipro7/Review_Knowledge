f50e8debacf6b1b8c85f3e4bc793aa7fb4202a77
jianhui.fang <jianhui.fang@ucarinc.com>
2020-06-09 17:01:43 +0800

    input url to render

--- JavaScript/Generator/test.js
+++ JavaScript/Generator/test.js
@@ -1,1 +1,9 @@
-const data = await 'asd'
+let name = 'antipro';
+function call(x, y) {
+  return name;
+}
+export const sqrt = Math.sqrt;
+export function square(x) {
+  return x * x;
+}
+export { name, call }
